9ae2ddcdb153114539b3aba0200b8190c46ff220
Nitin Negi <nitinnegi0810@gmail.com>
2019-07-06 22:31:15 +0530

    Updates

---
+++ precache-manifest.383e39a893846ba219d2ed13e7c8b7c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8665b2b063409d51ad99",
+    "url": "/tour-of-avengers/static/js/runtime~main.8665b2b0.js"
+  },
+  {
+    "revision": "d4b60f3c05696ad8ce71",
+    "url": "/tour-of-avengers/static/js/main.d4b60f3c.chunk.js"
+  },
+  {
+    "revision": "a2b3d365ef450eae1f3e",
+    "url": "/tour-of-avengers/static/js/1.a2b3d365.chunk.js"
+  },
+  {
+    "revision": "d4b60f3c05696ad8ce71",
+    "url": "/tour-of-avengers/static/css/main.5cd4cda4.chunk.css"
+  },
+  {
+    "revision": "d2607702da3ee87d3c60c999e1870f72",
+    "url": "/tour-of-avengers/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/tour-of-avengers/precache-manifest.383e39a893846ba219d2ed13e7c8b7c9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/tour-of-avengers/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
