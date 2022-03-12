47b309ae48905452db2664b37ad4e4f7cbe3d91a
Nitin Negi <nitin@ascendeum.com>
2022-03-12 16:58:05 +0530

    Image urls updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 export const Header = () => {
     return (
         <header className="app-header">
-            <img src='imgs/logo.jpg' alt='logo' />
+            <img src='/imgs/logo.jpg' alt='logo' />
             <h4>Tour of Avengers</h4>
         </header>
     );
